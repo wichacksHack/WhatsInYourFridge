04ef0689ed1ae80df6ca1834f73f2d05fd60dad7
Samridhi16 <samridhipramanik@gmail.com>
2022-02-27 09:45:42 -0500

    Add files via upload

---
+++ wiyf/mainapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# declare a new model with a name "GeeksModel"
+class AddModel(models.Model):
+    # fields of the model
+    # title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    # renames the instances of the model
+    # with their title name
+    # def __str__(self):
+    #     return self.title
